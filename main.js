0addcbfd07075246be3cf992627263699ee59081
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-04-24 14:52:45 +0200

    Catch 404 and add error handling

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
