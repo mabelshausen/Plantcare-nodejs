5872070986c9287554b868c955580f0e55636bd8
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-07 11:41:06 +0200

    Add add-plants migration

---
+++ migrations/20210507091059-add-plants.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('plants', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: { type: 'string', notNull: true },
+    sciName: 'string',
+    age: 'int',
+    room_id: {
+      type: 'int',
+      notNull: true,
+      foreignKey: {
+        name: 'plants_room_id_fk',
+        table: 'rooms',
+        rules: {
+          OnDelete: 'RESTRICT',
+          OnUpdate: 'CASCADE'
+        },
+        mapping: 'id'
+      }
+    }
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('plants');
+};
+
+exports._meta = {
+  "version": 1
+};
